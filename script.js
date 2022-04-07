7590994f41bea3da3f373f06ac9b7c7d19af4689
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-04-07 15:30:49 +0200

    scaffolding, base esercizio

---
+++ script.js
@@ -0,0 +1,16 @@
+/* <div class="team-card">
+            <div class="card-image">
+              <img
+                src="img/wayne-barnett-founder-ceo.jpg"
+                alt="Wayne Barnett"
+              />
+            </div>
+            <div class="card-text">
+              <h3>Wayne Barnett</h3>
+              <p>Founder & CEO</p>
+            </div>
+          </div>
+*/
+
+// innanzitutto, mi credo un array contenente gli oggetti, ovvero i membri del team
+
